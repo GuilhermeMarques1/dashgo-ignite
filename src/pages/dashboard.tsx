bd906a67f4a1f1051610dc8b4198afa438a5a086
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-21 00:59:23 -0300

    feat: Add initial Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
 import { Header } from "@/Components/Header";
+import { Sidebar } from "@/Components/Sidebar";
+import { Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW="1480" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
